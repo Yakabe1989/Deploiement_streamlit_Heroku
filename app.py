34f201f2e335b2ad464a07dff7be2ed93140e314
Yakabe1989 <113184713+Yakabe1989@users.noreply.github.com>
2022-10-27 18:44:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,6 @@ def Gauge(ID):
     API_URL = "https://donnees-dashboard.herokuapp.com/"
     response = requests.get(API_URL) #Chargement des données
     content = json.loads(response.content.decode('utf-8'))
-    df = pd.DataFrame(content)
     Decision = list(pd.Series(content['decision']).values)
     ID_client = list(pd.Series(content['SK_ID_CURR']).values)
     probabilite_client = list(pd.Series(content['probabilite_client']).values)
@@ -77,9 +76,9 @@ def Caracteristiques_client(ID):
     'EXT_SOURCE_2',
     'EXT_SOURCE_3',
     'prediction']
-    DF_CAR = df[valeur]
-    DF_CAR.loc[DF_CAR["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
-    Caracteristiques = DF_CAR.loc[DF_CAR['SK_ID_CURR']==ID]
+    df = df[valeur]
+    df.loc[df["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
+    Caracteristiques = df.loc[df['SK_ID_CURR']==ID]
     return Caracteristiques
 
 
@@ -103,10 +102,10 @@ def Caracteristiques_client_similaires(ID):
     'EXT_SOURCE_2',
     'EXT_SOURCE_3',
     'prediction']
-    DF_CAR = df[valeur]
-    DF_CAR.loc[DF_CAR["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
-    decision = list(DF_CAR.loc[DF_CAR['SK_ID_CURR'] == ID, 'prediction'])
-    Caracteristiques = DF_CAR.loc[DF_CAR['prediction'] == decision[0]]
+    df = df[valeur]
+    df.loc[df["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
+    decision = list(df.loc[df['SK_ID_CURR'] == ID, 'prediction'])
+    Caracteristiques = df.loc[df['prediction'] == decision[0]]
     return Caracteristiques.describe()
 
 
@@ -132,10 +131,10 @@ def Caracteristiques_client_opposes(ID):
     'EXT_SOURCE_2',
     'EXT_SOURCE_3',
     'prediction']
-    DF_CAR = df[valeur]
-    DF_CAR.loc[DF_CAR["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
-    decision = list(DF_CAR.loc[DF_CAR['SK_ID_CURR'] == ID,'prediction'])
-    Caracteristiques = DF_CAR.loc[DF_CAR['prediction'] != decision[0]]
+    df = df[valeur]
+    df.loc[df["Duree_emploi_en_annee_brut"] < 0, "Duree_emploi_en_annee_brut"] = 0
+    decision = list(df.loc[df['SK_ID_CURR'] == ID,'prediction'])
+    Caracteristiques = df.loc[df['prediction'] != decision[0]]
     return Caracteristiques.describe()
 
 
@@ -164,11 +163,6 @@ def graphique(ID,champs):
         fig.add_vline(x=valeur_id[0], line_width=3, line_dash="dash", line_color="green")
         graphique = st.plotly_chart(fig, use_container_width=True)
         return graphique
-
-
-
-
-
 
 
 
@@ -230,11 +224,12 @@ if st.sidebar.checkbox('Decision du credit', key=18):
 
 
 if st.sidebar.checkbox("Features importance", key=38):
-    # Afficher le graphique shap local et shap global - shap de l'individu moyen de la classe
+    # Afficher le graphique shap local
     st.header("Analyse des predictions du modele")
     data = pd.read_csv("./X_test_data_Dashboard.csv", sep=',')
     shap_data = data.reset_index(drop=True)
     shap_index = list(shap_data.index[shap_data['SK_ID_CURR'] == ID])
+
 
     st.subheader("modele local")
     fig2, ax = plt.subplots(nrows=1, ncols=1)
@@ -245,7 +240,7 @@ if st.sidebar.checkbox("Features importance", key=38):
     with st.expander("Description du modèle local"):
         st.write("""
                 ce graphique présente les variables discriminantes pour l'acceptation ou
-                le refus de credit pour un individu.en rouge, les champs favorisant une acceptation de credit.
+                le refus de credit pour un individu. en rouge, les champs favorisant une acceptation de credit.
                 En bleu, ceux favorisant un refus.
             """)
 
@@ -267,7 +262,13 @@ if st.sidebar.checkbox("Analyse visuelle", key=38):
     API_URL = "https://donnees-dashboard.herokuapp.com/"
     response = requests.get(API_URL)  # Chargement des données
     content = json.loads(response.content.decode('utf-8'))
-    Colonne = list(content.keys())
+    #Colonne = list(content.keys())
+    Colonne = ['EXT_SOURCE_1','EXT_SOURCE_2',
+    'EXT_SOURCE_3','Age_en_annee_brut',
+    'Annuite_mensuel_credit_apres_modelisation','Annuite_mensuel_credit_avant_modelisation',
+    'AMT_ANNUITY','AMT_CREDIT','AMT_GOODS_PRICE','AMT_INCOME_TOTAL',
+    'DAYS_BIRTH','DAYS_EMPLOYED','Duree_emploi_en_annee_brut','INCOME_PER_PERSON',
+    'Montant_credit_apres_modelisation','Montant_credit_avant_modelisation','Nombre_enfants']
     champs = st.selectbox(
         'Quel champ voulez vous analyser?',
         Colonne)
